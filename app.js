29de49749349796d0e0313cb554575a51c0bb840
Coding Raccoon <egamyakub357@gmail.com>
2024-07-28 15:55:46 +0500

    The project restructured to be developed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import http from "http";
 import fs from "fs/promises";
 import url from "url";
 import path from "path";
+import express from "express";
 
 // * Setting up the port number for localhost://
 const PORT = 8080;
@@ -11,23 +12,35 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__dirname);
 
-const server = http.createServer(async (req, res) => {
-  try {
-    if (req.method === "GET" && req.url === "/") {
-      let filePath = path.join(__dirname, "public", "index.html");
-      const data = await fs.readFile(filePath);
-      res.setHeader("COntent-Type", "text/html");
-      res.write(data);
-      res.end();
-    } else {
-      throw new Error("PAGE NOT FOUND\n404");
-    }
-  } catch (error) {
-    res.writeHead(500, { "Content-Type": "text/plain" });
-    res.end(`Server Error \n${error}`);
-  }
+// * Creating EXPRESS app
+const app = express();
+
+// * Using express middles to set-up public directory as static file directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// * Starting the server with express
+app.listen(PORT, () => {
+  console.log(`Server listening on port:): ${PORT}`);
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+// ! LEGACY CODE
+// const server = http.createServer(async (req, res) => {
+//   try {
+//     if (req.method === "GET" && req.url === "/") {
+//       let filePath = path.join(__dirname, "public", "index.html");
+//       const data = await fs.readFile(filePath);
+//       res.setHeader("COntent-Type", "text/html");
+//       res.write(data);
+//       res.end();
+//     } else {
+//       throw new Error("PAGE NOT FOUND\n404");
+//     }
+//   } catch (error) {
+//     res.writeHead(500, { "Content-Type": "text/plain" });
+//     res.end(`Server Error \n${error}`);
+//   }
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
